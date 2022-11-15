5fc07c08d1b3ebf019f2cf236e14d494e5ce3f3b
Andre Sousa <up202005277@up.pt>
2022-11-15 18:54:32 +0000

    Bids

---
+++ app/Http/Controllers/BidController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bid;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $bid = new Bid();
+        // $this->authorize('create', $card);
+
+        $bid->amount = $request->input('amount');
+        $bid->auction_id = $request->input('auction_id');
+        $bid->user_id = $request->input('user_id');
+        $bid->save();
+        $bid->name = DB::table('users')->find($request->input('user_id'))->value('name');
+
+        return $bid;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bid  $bid
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bid $bid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bid  $bid
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bid $bid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bid  $bid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bid $bid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bid  $bid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bid $bid)
+    {
+        //
+    }
+}
--- resources/views/partials/auction_details.blade.php
+++ resources/views/partials/auction_details.blade.php
@@ -4,5 +4,15 @@
     <h5>Enddate: {{$details->end_date}}</h5>
     <h5>Current highest bid: </h5> @include('partials.bid', ['bid' => $bids->max()])
     <h5>Buy now: {{$details->buy_now}}</h5>
-    @each('partials.bid', $bids, 'bid', 'partials.no_items')
+    <form>
+        <input id="user_id" hidden value="{{Auth::id()}}">
+        <label for="bid_amount"> Make bid: </label>
+        <input type="number" id="bid_amount" name="amount">
+    </form>
+
+    <button id="make_bid">Make bid</button>
+    <div id="bids_list">
+        @each('partials.bid', $bids, 'bid', 'partials.no_items')
+    </div>
+
 </div>
