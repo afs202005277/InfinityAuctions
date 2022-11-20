f446d0c3be078979ee693917f1c821ac5d1fee40
Pedro Moreira <pedromoreirah3@gmail.com>
2022-11-20 10:55:43 +0000

    verfication errors

--- public/js/user_profile.js
+++ public/js/user_profile.js
@@ -1,13 +1,39 @@
-// document.querySelectorAll(".info_bar").forEach((section) => {
-    
-//     section.querySelector("h5").onclick = () => {
-//         console.log(section);
-//         div = document.querySelector(".info_content");
-//         if(div.style.display === 'none'){
-//             div.style.display= 'block';
-//         }else {
-//             div.style.display = 'none';
-//         }
-//     };
-//   });
-  
+document.querySelector("#info_bar_1").onclick = function() {
+        document.querySelector("#change_data").style.display = "block";
+        document.querySelector("#auctions_owned").style.display = "none";
+        document.querySelector("#bids_placed").style.display = "none";
+        document.querySelector("#bidding_auctions").style.display = "none";
+        document.querySelector("#following_auction").style.display = "none";
+    };
+
+document.querySelector("#info_bar_2").onclick = function() {
+        document.querySelector("#change_data").style.display = "none";
+        document.querySelector("#auctions_owned").style.display = "block";
+        document.querySelector("#bids_placed").style.display = "none";
+        document.querySelector("#bidding_auctions").style.display = "none";
+        document.querySelector("#following_auction").style.display = "none";
+    };
+
+document.querySelector("#info_bar_3").onclick = function() {
+        document.querySelector("#change_data").style.display = "none";
+        document.querySelector("#auctions_owned").style.display = "none";
+        document.querySelector("#bids_placed").style.display = "block";
+        document.querySelector("#bidding_auctions").style.display = "none";
+        document.querySelector("#following_auction").style.display = "none";
+    };
+
+document.querySelector("#info_bar_4").onclick = function() {
+        document.querySelector("#change_data").style.display = "none";
+        document.querySelector("#auctions_owned").style.display = "none";
+        document.querySelector("#bids_placed").style.display = "none";
+        document.querySelector("#bidding_auctions").style.display = "block";
+        document.querySelector("#following_auction").style.display = "none";
+    };
+
+document.querySelector("#info_bar_5").owned = function() {
+        document.querySelector("#change_data").style.display = "none";
+        document.querySelector("#auctions_owned").style.display = "none";
+        document.querySelector("#bids_placed").style.display = "none";
+        document.querySelector("#bidding_auctions").style.display = "none";
+        document.querySelector("#following_auction").style.display = "block";
+    };
--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -18,30 +18,50 @@
         </div>
     </div>
     <div class="row">
-        <h4 class="info_bar_1"> Change Data </h4>
-        <h4 class="info_bar_1"> Owned Auctions </h4>
-        <h4 class="info_bar_1"> Bids Placed </h4>
-        <h4 class="info_bar_1"> Bidding Auction </h4>
-        <h4 class="info_bar_1"> Following </h4>
+        <h4 id="info_bar_1"> Change Data </h4>
+        <h4 id="info_bar_2"> Owned Auctions </h4>
+        <h4 id="info_bar_3"> Bids Placed </h4>
+        <h4 id="info_bar_4"> Bidding Auction </h4>
+        <h4 id="info_bar_5"> Following Auction</h4>
     </div>
     <hr/>
-    <!--<div class="auctions_owned">
-        @if($user->ownedAuctions!=null)
+    <!-- Change Data -->
+    <div id="change_data">
+        @include('partials.edit_profile')
+    </div>
+
+    <!-- Owned Auctions -->
+    <div id="auctions_owned">
+        <p> {{ $user->ownedAuctions()->get() }}</p>
+        @if($user->ownedAuctions()->get()!== null)
             @foreach ($user->ownedAuctions as $auction) 
                 @include('partials.auctions_owned', compact('auction'))
             @endforeach
+        @else
+            <p> This user doesn't own any Auction ! </p>
         @endif
-    </div>
-    
-    
-    @include('partials.edit_profile') -->
-    <p> {{$user->bids}} </p>
-    <div class="auctions_owned">
+    </div> 
+     
+    <!-- Bids Placed -->
+    <div id="bids_placed">
         @if($user->ownedAuctions!=null)
             @foreach ($user->bids as $bid) 
                 @include('partials.placed_bids', compact('bid'))
             @endforeach
         @endif
-    </div>    
+    </div>
+
+    <!-- Bidding Auction -->
+    <div id="bidding_auctions">
+        @if($user->ownedAuctions!=null)
+            @foreach ($user->bids as $bid) 
+                @include('partials.placed_bids', compact('bid'))
+            @endforeach
+        @endif
+    </div>
+    
+    <div id="following_auction">
+        <p> $user->followedAuctions </p>
+    </p>
    
 @endsection
