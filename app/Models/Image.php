15e5034a52138ff3cb294d6c0776cc1421198a5c
Andre Sousa <up202005277@up.pt>
2022-11-21 18:27:48 +0000

    Added table

---
+++ app/Models/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $table = 'image';
+
+    public $timestamps = false;
+
+    public function user(){
+        return $this->hasOne(User::class, 'profile_image');
+    }
+
+    public function auction(){
+        return $this->belongsTo(Auction::class, 'auction_id');
+    }
+}
--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -10,6 +10,7 @@ DROP TABLE IF EXISTS report;
 DROP TABLE IF EXISTS report_option;
 DROP TABLE IF EXISTS auction;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS image;
 
 DROP TYPE IF EXISTS notification_type;
 DROP TYPE IF EXISTS penalty_type;
@@ -24,88 +25,108 @@ CREATE TYPE auction_possible_state AS ENUM ('Cancelled', 'Running', 'To be start
 CREATE TYPE penalty_type AS ENUM ('3 day ban', '5 day ban', '10 day ban', '1 month ban', 'Banned for life');
 CREATE TYPE gender_possible AS ENUM ('M', 'F', 'NB', 'O');
 
-CREATE TABLE IF NOT EXISTS users (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL,
-    gender gender_possible,
-	cellphone CHAR(9) UNIQUE,
-	email VARCHAR(320) UNIQUE,
-    birth_date DATE NOT NULL,
-	address VARCHAR(255) UNIQUE,
-	password VARCHAR NOT NULL,
-	rate REAL,
-	credits REAL,
-	wishlist TEXT [],
-	is_admin BOOLEAN NOT NULL,
-	CONSTRAINT valid_rate CHECK (rate >= 0 AND rate <= 5),
+
+CREATE TABLE IF NOT EXISTS image
+(
+    id         SERIAL PRIMARY KEY,
+    path       TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id            SERIAL PRIMARY KEY,
+    name          VARCHAR(30) NOT NULL,
+    gender        gender_possible,
+    cellphone     CHAR(9) UNIQUE,
+    email         VARCHAR(320) UNIQUE,
+    birth_date    DATE        NOT NULL,
+    address       VARCHAR(255) UNIQUE,
+    password      VARCHAR     NOT NULL,
+    rate          REAL,
+    credits       REAL,
+    wishlist      TEXT[],
+    is_admin      BOOLEAN     NOT NULL,
+    profile_image INTEGER REFERENCES image DEFAULT 0,
+    CONSTRAINT valid_rate CHECK (rate >= 0 AND rate <= 5),
     CONSTRAINT valid_birth CHECK (birth_date between '1900-01-01' and now() - interval '18 years')
 );
 
-CREATE TABLE IF NOT EXISTS auction (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	description TEXT NOT NULL,
-	base_price REAL NOT NULL,
-	start_date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-	end_date TIMESTAMP WITH TIME ZONE NOT NULL,
-	buy_now REAL,
-	state auction_possible_state NOT NULL,
-	auction_owner_id INTEGER REFERENCES users ON UPDATE CASCADE NOT NULL,
-	CONSTRAINT valid_dates CHECK (start_date < end_date)
+CREATE TABLE IF NOT EXISTS auction
+(
+    id               SERIAL PRIMARY KEY,
+    name             TEXT                                       NOT NULL,
+    description      TEXT                                       NOT NULL,
+    base_price       REAL                                       NOT NULL,
+    start_date       TIMESTAMP WITH TIME ZONE DEFAULT now()     NOT NULL,
+    end_date         TIMESTAMP WITH TIME ZONE                   NOT NULL,
+    buy_now          REAL,
+    state            auction_possible_state                     NOT NULL,
+    auction_owner_id INTEGER REFERENCES users ON UPDATE CASCADE NOT NULL,
+    CONSTRAINT valid_dates CHECK (start_date < end_date)
 );
 
-CREATE TABLE IF NOT EXISTS bid (
-	id SERIAL PRIMARY KEY,
-	date TIMESTAMP WITH TIME ZONE DEFAULT now()::timestamp(0) NOT NULL,
- 	amount REAL NOT NULL,
-	user_id INTEGER REFERENCES users ON UPDATE CASCADE NOT NULL,
-	auction_id INTEGER REFERENCES auction ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+ALTER TABLE image ADD COLUMN auction_id INTEGER REFERENCES auction;
+
+CREATE TABLE IF NOT EXISTS bid
+(
+    id         SERIAL PRIMARY KEY,
+    date       TIMESTAMP WITH TIME ZONE DEFAULT now()::timestamp(0)           NOT NULL,
+    amount     REAL                                                           NOT NULL,
+    user_id    INTEGER REFERENCES users ON UPDATE CASCADE                     NOT NULL,
+    auction_id INTEGER REFERENCES auction ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS report (
-	id SERIAL PRIMARY KEY,
-	date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-	penalty penalty_type,
-	reported_user INTEGER REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE,
-	reporter INTEGER REFERENCES users ON UPDATE CASCADE NOT NULL,
-	auction_reported INTEGER REFERENCES auction ON UPDATE CASCADE,
-	admin_id INTEGER REFERENCES users ON UPDATE CASCADE,
-	CONSTRAINT no_self_reports CHECK (reported_user != reporter)
+CREATE TABLE IF NOT EXISTS report
+(
+    id               SERIAL PRIMARY KEY,
+    date             TIMESTAMP WITH TIME ZONE DEFAULT now()     NOT NULL,
+    penalty          penalty_type,
+    reported_user    INTEGER REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE,
+    reporter         INTEGER REFERENCES users ON UPDATE CASCADE NOT NULL,
+    auction_reported INTEGER REFERENCES auction ON UPDATE CASCADE,
+    admin_id         INTEGER REFERENCES users ON UPDATE CASCADE,
+    CONSTRAINT no_self_reports CHECK (reported_user != reporter)
 );
 
-CREATE TABLE IF NOT EXISTS notification (
-	id SERIAL PRIMARY KEY,
-	date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-	TYPE notification_type NOT NULL,
-	user_id INTEGER REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
-	auction_id INTEGER REFERENCES auction ON UPDATE CASCADE,
-	report_id INTEGER REFERENCES report ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS notification
+(
+    id         SERIAL PRIMARY KEY,
+    date       TIMESTAMP WITH TIME ZONE DEFAULT now()                       NOT NULL,
+    TYPE       notification_type                                            NOT NULL,
+    user_id    INTEGER REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    auction_id INTEGER REFERENCES auction ON UPDATE CASCADE,
+    report_id  INTEGER REFERENCES report ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS category (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS category
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS auction_category (
-	category_id INTEGER REFERENCES category ON UPDATE CASCADE,
-	auction_id INTEGER REFERENCES auction ON UPDATE CASCADE,
-	PRIMARY KEY (category_id, auction_id)
+CREATE TABLE IF NOT EXISTS auction_category
+(
+    category_id INTEGER REFERENCES category ON UPDATE CASCADE,
+    auction_id  INTEGER REFERENCES auction ON UPDATE CASCADE,
+    PRIMARY KEY (category_id, auction_id)
 );
 
-CREATE TABLE IF NOT EXISTS following (
-	user_id INTEGER REFERENCES users ON UPDATE CASCADE,
-	auction_id INTEGER REFERENCES auction ON UPDATE CASCADE,
-	PRIMARY KEY (user_id, auction_id)
+CREATE TABLE IF NOT EXISTS following
+(
+    user_id    INTEGER REFERENCES users ON UPDATE CASCADE,
+    auction_id INTEGER REFERENCES auction ON UPDATE CASCADE,
+    PRIMARY KEY (user_id, auction_id)
 );
 
-CREATE TABLE IF NOT EXISTS report_option (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS report_option
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS report_reasons (
-	id_report_option INTEGER REFERENCES report_option ON UPDATE CASCADE,
-	id_report INTEGER REFERENCES report ON UPDATE CASCADE,
-	PRIMARY KEY (id_report_option, id_report)
+CREATE TABLE IF NOT EXISTS report_reasons
+(
+    id_report_option INTEGER REFERENCES report_option ON UPDATE CASCADE,
+    id_report        INTEGER REFERENCES report ON UPDATE CASCADE,
+    PRIMARY KEY (id_report_option, id_report)
 );
