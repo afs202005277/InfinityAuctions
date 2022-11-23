7d8f2daf23da753e99531aec9dd82311f1a08341
Andre Sousa <up202005277@up.pt>
2022-11-23 23:16:06 +0000

    settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,13 @@ class AppServiceProvider extends ServiceProvider
         View::composer('layouts.app', function($view) {
             $view->with('categories', Category::all());
         });
+
+        if(env('FORCE_HTTPS',false)) {
+            error_log('configuring https');
+            $app_url = config("app.url");
+            URL::forceRootUrl($app_url);
+            $schema = explode(':', $app_url)[0];
+            URL::forceScheme($schema);
+        }
     }
 }
