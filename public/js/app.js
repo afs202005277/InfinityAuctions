4d232e0bc15a112868acd23aaf6b8121b53df44c
Andre Sousa <up202005277@up.pt>
2022-11-24 21:15:15 +0000

    removed thingy code

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,7 @@
 function addEventListeners() {
-    let itemCheckers = document.querySelectorAll('article.card li.item input[type=checkbox]');
-    [].forEach.call(itemCheckers, function (checker) {
-        checker.addEventListener('change', sendItemUpdateRequest);
-    });
-
-    let itemCreators = document.querySelectorAll('article.card form.new_item');
-    [].forEach.call(itemCreators, function (creator) {
-        creator.addEventListener('submit', sendCreateItemRequest);
-    });
-
-    let itemDeleters = document.querySelectorAll('article.card li a.delete');
-    [].forEach.call(itemDeleters, function (deleter) {
-        deleter.addEventListener('click', sendDeleteItemRequest);
-    });
-
-    let cardDeleters = document.querySelectorAll('article.card header a.delete');
-    [].forEach.call(cardDeleters, function (deleter) {
-        deleter.addEventListener('click', sendDeleteCardRequest);
-    });
-
-    let filterCheckers = document.querySelectorAll('#search-filter input[type=checkbox]');
-    [].forEach.call(filterCheckers, function(filterChecker) {
-        filterChecker.addEventListener('change', modifyFiltersRequest);
-    });
-
-    let auctionCreator = document.querySelector('#make_bid');
-    if (auctionCreator != null)
-        auctionCreator.addEventListener('click', sendCreateBidRequest);
+    let bidCreator = document.querySelector('#make_bid');
+    if (bidCreator != null)
+        bidCreator.addEventListener('click', sendCreateBidRequest);
 }
 
 function encodeForAjax(data) {
@@ -46,36 +21,6 @@ function sendAjaxRequest(method, url, data, handler) {
     request.send(encodeForAjax(data));
 }
 
-function sendItemUpdateRequest() {
-    let item = this.closest('li.item');
-    let id = item.getAttribute('data-id');
-    let checked = item.querySelector('input[type=checkbox]').checked;
-
-    sendAjaxRequest('post', '/api/item/' + id, {done: checked}, itemUpdatedHandler);
-}
-
-function sendDeleteItemRequest() {
-    let id = this.closest('li.item').getAttribute('data-id');
-
-    sendAjaxRequest('delete', '/api/item/' + id, null, itemDeletedHandler);
-}
-
-function sendCreateItemRequest(event) {
-    let id = this.closest('article').getAttribute('data-id');
-    let description = this.querySelector('input[name=description]').value;
-
-    if (description != '')
-        sendAjaxRequest('put', '/api/cards/' + id, {description: description}, itemAddedHandler);
-
-    event.preventDefault();
-}
-
-function sendDeleteCardRequest(event) {
-    let id = this.closest('article').getAttribute('data-id');
-
-    sendAjaxRequest('delete', '/api/cards/' + id, null, cardDeletedHandler);
-}
-
 function sendCreateBidRequest(event) {
     let amount = document.querySelector('#bid_amount').value;
     let user_id = document.querySelector('#user_id').value;
@@ -89,59 +34,6 @@ function sendCreateBidRequest(event) {
         }, bidAddedHandler);
     document.querySelector('#bid_amount').value = "";
     event.preventDefault();
-}
-
-function modifyFiltersRequest() {
-  let oldUrlParams = new URLSearchParams(window.location.search);
-
-  let newUrlParams = new URLSearchParams();
-  if ( oldUrlParams.has('search') ) {
-    newUrlParams.append('search', oldUrlParams.get('search'));
-  }
-
-  let checked = document.querySelectorAll('#search-filter input[type=checkbox]:checked');
-  for (let i = 0; i < checked.length; i++) {
-    newUrlParams.append(checked[i].getAttribute("name") + '[' + i + ']', checked[i].getAttribute("value"));
-  }
-
-  window.location.href = window.location.pathname + '?' + newUrlParams;
-}
-
-function itemUpdatedHandler() {
-    let item = JSON.parse(this.responseText);
-    let element = document.querySelector('li.item[data-id="' + item.id + '"]');
-    let input = element.querySelector('input[type=checkbox]');
-    element.checked = item.done === "true";
-}
-
-function itemAddedHandler() {
-    if (this.status !== 200) window.location = '/';
-    let item = JSON.parse(this.responseText);
-
-    // Create the new item
-    let new_item = createItem(item);
-
-    // Insert the new item
-    let card = document.querySelector('article.card[data-id="' + item.card_id + '"]');
-    let form = card.querySelector('form.new_item');
-    form.previousElementSibling.append(new_item);
-
-    // Reset the new item form
-    form.querySelector('[type=text]').value = "";
-}
-
-function itemDeletedHandler() {
-    if (this.status !== 200) window.location = '/';
-    let item = JSON.parse(this.responseText);
-    let element = document.querySelector('li.item[data-id="' + item.id + '"]');
-    element.remove();
-}
-
-function cardDeletedHandler() {
-    if (this.status !== 200) window.location = '/';
-    let card = JSON.parse(this.responseText);
-    let article = document.querySelector('article.card[data-id="' + card.id + '"]');
-    article.remove();
 }
 
 function bidAddedHandler() {
@@ -174,22 +66,6 @@ function createBidInfo(bid) {
     new_bid_info.innerHTML = `<span>${bid.name}</span> - ${parseDate(bid.date)}`;
 
     return new_bid_info;
-}
-
-function createItem(item) {
-    let new_item = document.createElement('li');
-    new_item.classList.add('item');
-    new_item.setAttribute('data-id', item.id);
-    new_item.innerHTML = `
-  <label>
-    <input type="checkbox"> <span>${item.description}</span><a href="#" class="delete">&#10761;</a>
-  </label>
-  `;
-
-    new_item.querySelector('input').addEventListener('change', sendItemUpdateRequest);
-    new_item.querySelector('a.delete').addEventListener('click', sendDeleteItemRequest);
-
-    return new_item;
 }
 
 addEventListeners();
