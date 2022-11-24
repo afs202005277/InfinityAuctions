47a155e0e922c5cbd25591432586bedf14166c17
Pedro Fonseca <pedrojgfonseca@gmail.com>
2022-11-24 00:01:12 +0000

    Fixed bidding when auction isnt active and css

--- public/js/auctions_timer.js
+++ public/js/auctions_timer.js
@@ -1,55 +1,62 @@
 let countDownDate = new Date(document.getElementById("final-date").textContent).getTime();
 
-function updateButtons(bid){
+function updateButtons(bid) {
     let buttons = document.querySelectorAll('.price-suggestions form button');
     buttons[0].textContent = ((parseFloat(bid.amount) * 1.10).toFixed(2)).toString() + '€';
     buttons[1].textContent = ((parseFloat(bid.amount) * 1.25).toFixed(2)).toString() + '€';
     buttons[2].textContent = ((parseFloat(bid.amount) * 1.50).toFixed(2)).toString() + '€';
 }
 
-function bidsReceivedHandler(){
+function disableButtons() {
+    let buttons = document.querySelectorAll('.price-suggestions form button');
+    buttons.foreach(button => {
+        button.disabled = true;
+    }
+}
+
+function bidsReceivedHandler() {
     let bids = JSON.parse(this.responseText);
-    if (bids.length !== 0){
+    if (bids.length !== 0) {
         document.querySelector('#bids_list').innerHTML = "";
         document.querySelector('.bid-amount').innerHTML = createBidAmount(bids[0]).innerHTML;
         document.querySelector('.info-bid').innerHTML = createBidInfo(bids[0]).innerHTML;
         updateButtons(bids[0]);
 
-        for (let i=1;i<bids.length;i++){
+        for (let i = 1; i < bids.length; i++) {
             document.querySelector('#bids_list').appendChild(createBidAmount(bids[i]));
             document.querySelector('#bids_list').appendChild(createBidInfo(bids[i]));
         }
     }
 }
 
-function buttonsSuggestionsListener(){
+function buttonsSuggestionsListener() {
     let buttons = document.querySelectorAll('.price-suggestions form button');
-    for (let button of buttons){
-        button.addEventListener('click', function (event){
+    for (let button of buttons) {
+        button.addEventListener('click', function (event) {
             event.preventDefault();
             document.querySelector('#bid_amount').value = button.textContent.substring(0, button.textContent.lastIndexOf('€'));
         })
     }
 }
 
-let x = setInterval(function() {
+let x = setInterval(function () {
 
-  let now = new Date().getTime();
+    let now = new Date().getTime();
 
-  let distance = countDownDate - now;
+    let distance = countDownDate - now;
 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("final-date").innerHTML = days + "D:" + hours + "H:"
-  + minutes + "M:" + seconds + "S";
+    document.getElementById("final-date").innerHTML = days + "D:" + hours + "H:"
+        + minutes + "M:" + seconds + "S";
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("final-date").innerHTML = "AUCTION ENDED";
-  }
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("final-date").innerHTML = "AUCTION ENDED";
+    }
 
     let auction_id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1, window.location.href.length);
     sendAjaxRequest('get', '/api/auctions/getAllBids/' + auction_id, {}, bidsReceivedHandler);
--- resources/views/partials/auction_details.blade.php
+++ resources/views/partials/auction_details.blade.php
@@ -20,38 +20,38 @@
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$bids->max()->amount*1.10, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$bids->max()->amount*1.10, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$bids->max()->amount*1.10, 2, '.', '')}}€</button>
             </form>
             {{-- Price Suggest 2 --}}
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$bids->max()->amount*1.25, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$bids->max()->amount*1.25, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$bids->max()->amount*1.25, 2, '.', '')}}€</button>
             </form>
             {{-- Price Suggest 3 --}}
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$bids->max()->amount*1.50, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$bids->max()->amount*1.50, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$bids->max()->amount*1.50, 2, '.', '')}}€</button>
             </form>
         @else
             {{-- Price Suggest 1 --}}
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$details->base_price, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$details->base_price*1, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$details->base_price*1, 2, '.', '')}}€</button>
             </form>
             {{-- Price Suggest 2 --}}
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$details->base_price*1.10, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$details->base_price*1.10, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$details->base_price*1.10, 2, '.', '')}}€</button>
             </form>
             {{-- Price Suggest 3 --}}
             <form>
                 <input id="user_id" hidden value="{{Auth::id()}}">
                 <input type="number" hidden id="bid_amount" name="amount" placeholder="Bid Amount" value="{{number_format((float)$details->base_price*1.25, 2, '.', '')}}">
-                <button type="submit">{{number_format((float)$details->base_price*1.25, 2, '.', '')}}€</button>
+                <button type="submit" disabled="@php if ($details->state != "Running") { echo "disabled"; } @endphp">{{number_format((float)$details->base_price*1.25, 2, '.', '')}}€</button>
             </form>
         @endif
     </section>
