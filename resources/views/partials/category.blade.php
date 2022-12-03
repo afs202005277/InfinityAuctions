5cb9af48579701f43375921bf41f88684557b904
Vitor Cavaleiro <up202004724@fe.up.pt>
2022-12-03 16:14:29 +0000

    Minor correction

--- resources/views/partials/category.blade.php
+++ resources/views/partials/category.blade.php
@@ -1,1 +1,1 @@
-<h4><a href="/search?category%5B0%5D={{ $category->id }}">{{ $category->name }}</a></h4>
+<h4><a href="/search?filter%5Bcategory%5D%5B0%5D={{ $category->id }}">{{ $category->name }}</a></h4>
