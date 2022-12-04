eb8c9f60b579fe54bf45592cca2826560e3cc07b
Pedro Moreira <pedromoreirah3@gmail.com>
2022-12-04 10:19:42 +0000

    New banner

--- resources/views/pages/main_page.blade.php
+++ resources/views/pages/main_page.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Home Page')
 
 @section('content')
+
     @include('partials.selected_auctions', ['selected_auctions' => $selectedAuctions])
     @include('partials.most_active', ['most_active' => $mostActive])
     <h2>CATEGORIES</h2>
