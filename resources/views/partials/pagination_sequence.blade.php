166b0ba581f30e4546f53a5160fb27623cbb849c
Pedro Moreira <pedromoreirah3@gmail.com>
2022-12-09 14:19:53 +0000

    Final User pagination

--- resources/views/partials/pagination_sequence.blade.php
+++ resources/views/partials/pagination_sequence.blade.php
@@ -2,13 +2,15 @@
 
 <div class="pagination p1">
       <ul>
+        @if($paginator->currentPage() != 1)
         <a href="{{url($paginator->previousPageUrl())}}"><li><</li></a>
-        <a class="is-active" href="#"><li>1</li></a>
-        <a href="#"><li>2</li></a>
-        <a href="#"><li>3</li></a>
-        <a href="#"><li>4</li></a>
-        <a href="#"><li>5</li></a>
-        <a href="#"><li>6</li></a>
+        @endif
+        <a class="is-active" href="#"><li>{{$paginator->currentPage()}}</li></a>
+        <a href="{{url($paginator->url($paginator->currentPage()+1))}}"><li>{{$paginator->currentPage()+1}}</li></a>
+        <a href="{{url($paginator->url($paginator->currentPage()+2))}}"><li>{{$paginator->currentPage()+2}}</li></a>
+        <a href="{{url($paginator->url($paginator->currentPage()+3))}}"><li>{{$paginator->currentPage()+3}}</li></a>
+        <a href="{{url($paginator->url($paginator->currentPage()+4))}}"><li>{{$paginator->currentPage()+4}}</li></a>
+        <a href="{{url($paginator->url($paginator->currentPage()+5))}}"><li>{{$paginator->currentPage()+5}}</li></a>
         <a href="{{url($paginator->nextPageUrl())}}"><li>></li></a>
       </ul>
     </div>
