a9edbffc1f967e0991ad6a04046aab6ae42aa52a
Andre Sousa <up202005277@up.pt>
2023-01-03 00:44:12 +0000

    Improved rate

--- public/js/rate_seller.js
+++ public/js/rate_seller.js
@@ -14,6 +14,7 @@ function rateAdded() {
     } else {
         document.querySelector('#popup').className = "";
         document.querySelector('#popup').style.display = 'none';
+        window.location.reload();
     }
 }
 
